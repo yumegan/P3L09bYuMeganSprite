ac7a9da48471db0d1f412eef7fc4b7b4ce01f95c
yumegan <yu.megan@gmail.com>
2022-04-21 14:13:52 -0400

    2 sprites... inspiration from nature...

--- app/src/main/java/com/example/p3l09byumegansprite/DrawView.java
+++ app/src/main/java/com/example/p3l09byumegansprite/DrawView.java
@@ -1,19 +1,40 @@
 package com.example.p3l09byumegansprite;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DrawView extends View {
-    Sprite sprite;
+    Sprite girl;
+    Sprite boy;
+    List<Sprite> kids = new ArrayList<>();
+    Paint paint=new Paint();
+    private float width, height;
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
-        sprite = new Sprite();
+        width = getWidth();
+
+        girl = new Sprite(0,0,10,10,6,10,Color.DKGRAY);
+        boy = new Sprite(width/2,height/2,width/2+10,height/2+10,-5,-7,Color.DKGRAY);
+
+        girl.grow(200);
+        girl.setBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.spritexb_7313));
+
+        boy.grow(200);
+        boy.setBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.spritexb_2204));
     }
 
     public DrawView(Context context, @Nullable AttributeSet attrs) {
@@ -23,8 +44,32 @@ public class DrawView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        sprite.update();
-        sprite.draw(canvas);
+        boy.update(canvas);
+        girl.update(canvas);
+
+        for (int i = 0; i < kids.size(); i++)
+            kids.get(i).update(canvas);
+
+
+        if(RectF.intersects(boy, girl)){
+            boy.setdX(boy.getdX()*-1);
+            boy.setdY(boy.getdY()*-1);
+
+            girl.setdX(girl.getdX()*-1);
+            girl.setdY(girl.getdY()*-1);
+
+            Sprite kid = new Sprite(girl.getX(),girl.getY(),girl.getX()+10,girl.getY()+10,2,3,Color.DKGRAY);
+            kid.grow(75);
+            kid.setBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.spritexb_6941));
+            kids.add(kid);
+        }
+
+        boy.draw(canvas);
+        girl.draw(canvas);
+
+        for (int i = 0; i < kids.size(); i++)
+            kids.get(i).draw(canvas);
+
         invalidate();
     }
 }
--- app/src/main/java/com/example/p3l09byumegansprite/Sprite.java
+++ app/src/main/java/com/example/p3l09byumegansprite/Sprite.java
@@ -1,39 +1,80 @@
 package com.example.p3l09byumegansprite;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.RectF;
 
-public class Sprite extends RectF {
-    private int dX, dY, color;
+class Sprite extends RectF {
+    private static final int BMP_COLUMNS = 4;
+    private static final int BMP_ROWS = 4;
+    private static final int DOWN=0, LEFT=1, RIGHT=2, UP=3;
+    private int ex, why, dX, dY, color;
+    private Bitmap bitmap;
+    private int currentFrame=0, iconWidth, iconHeight, animationDelay=20;
+    public Sprite() {
+        this(1,2, Color.RED);
+    }
+    public Sprite(int dX, int dY, int color) {
+        this(1,1,11,11,dX,dY,color);
+    }
+
+    public Sprite(float left, float top, float right, float bottom) {
+        this(left, top, right, bottom,1,2,Color.RED);
+    }
+    public Sprite(RectF r){
+        super(r);
+    }
     public Sprite(float left, float top, float right, float bottom, int dX, int dY, int color) {
         super(left, top, right, bottom);
         this.dX = dX;
         this.dY = dY;
         this.color = color;
+        this.ex = (int)left;
+        this.why= (int)top;
     }
-
-    public Sprite(float left, float top, float right, float bottom) {
-        this(left, top, right, bottom, 1, 2, Color.BLUE);   // passes to above constructor
-    }
-
-    public Sprite(int dX, int dY, int color) {
-        this(1, 1, 110, 110, dX, dY, color);
-    }
-
-    public Sprite() {
-        this(2,3,Color.GREEN);
-    }
-
-    public void update(){
-        offset(dX, dY);
+    public void update(Canvas canvas){
+        if(left+dX<0||right+dX>canvas.getWidth())//if next step hits boundary
+            dX*=-1; //bounce off left and right boundaries
+        if(bottom+dY>canvas.getHeight() || top+dY<0)
+            dY*=-1;
+//        if(top+dY>canvas.getHeight())//if next step puts off bottom of screen
+//            offsetTo(left,-height());//teleport to top of screen
+//        if(bottom+dY<0)
+//            offsetTo(left,canvas.getHeight());
+        offset(dX,dY);//moves dX to the right and dY downwards
+        ex+=dX;
+        why+=dY;
+        if(animationDelay--<0) {//increment to next sprite image after delay
+            currentFrame = ++currentFrame % BMP_COLUMNS;//cycles current image with boundary proteciton
+            animationDelay=20;//arbitrary delay before cycling to next image
+        }
     }
 
     public void draw(Canvas canvas){
-        Paint paint = new Paint();
-        paint.setColor(color);
-        canvas.drawCircle(centerX(), centerY(), width()/2, paint);
+        if(bitmap==null) {//if no bitmap exists draw a red circle
+            Paint paint = new Paint();
+            paint.setColor(color);//sets its color
+            canvas.drawCircle(centerX(), centerY(), width() / 2, paint);//draws circle
+        }else {
+            iconWidth = bitmap.getWidth() / BMP_COLUMNS;//calculate width of 1 image
+            iconHeight = bitmap.getHeight() / BMP_ROWS; //calculate height of 1 image
+            int srcX = currentFrame * iconWidth;       //set x of source rectangle inside of bitmap based on current frame
+            int srcY = getAnimationRow() * iconHeight; //set y to row of bitmap based on direction
+            Rect src = new Rect(srcX, srcY, srcX + iconWidth, srcY + iconHeight);  //defines the rectangle inside of heroBmp to displayed
+            canvas.drawBitmap(bitmap,src, this,null); //draw an image
+        }
+    }
+
+    private int getAnimationRow() {
+        if (Math.abs(dX)>Math.abs(dY)){         //if magnitude of x is bigger than magnitude y
+            if(Math.abs(dX)==dX) return RIGHT;  //if x is positive return row 2 for right
+            else return LEFT;                          //if x is negative return row 1 for left
+        } else if(Math.abs(dY)==dY) return DOWN;      //if y is positive return row 0 for up
+        else return UP;                                 //if y is positive return row 3 for up
+
     }
 
     public int getdX() {
@@ -52,11 +93,32 @@ public class Sprite extends RectF {
         this.dY = dY;
     }
 
+    public int getX(){
+        return this.ex;
+    }
+
+    public int getY(){
+        return this.why;
+    }
+
     public int getColor() {
         return color;
     }
 
     public void setColor(int color) {
         this.color = color;
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmap = bitmap;
+    }
+
+    public void grow(int i) {
+        right=right+i;
+        bottom=bottom+i;
     }
 }
